dfb22f67dced9b112361fadae521451d7c1b5c21
Esmat Najafi <145345735+Esmatnajafi@users.noreply.github.com>
2024-04-08 07:43:05 +0200

    Add

--- DropDown/src/Components/esmat/DropDownEs.tsx
+++ DropDown/src/Components/esmat/DropDownEs.tsx
@@ -1,43 +1,27 @@
 import React, { useState } from "react";
 
-const DropDownEs: React.FC = () => {
-  const [subject, setSubject] = useState<string[]>([
-    "facebook",
-    "Instagram",
-    "Tiktok",
-    "X",
-    "Whatsapp",
-  ]);
-  const [selectedOption, setSelectedOption] = useState<string>();
+interface IOptions {
+  value: string;
+  options: string[];
+}
+interface IDropdownsProps extends React.HTMLAttributes<HTMLDivElement> {
+    label?: string;
+    options: IOptions[];
+    value?: string;
+}
+export const DropDownEs = (props: IDropdownsProps) => {
+   const {label, options, value} = props;
 
   return (
     <>
       <div className="dropdown-container">
-        <div className="card-header">
-          <h2>Dropdown</h2>
-        </div>
-        <div className="card-body">
-          <form className="form-card">
-            <select className={`select-option ${selectedOption ? "selected" : ""}`}
-              name=""
-              id="form-select"
-              value={selectedOption}
-              onChange={(e) => setSelectedOption(e.target.value)}
-            >
-              <option className="placeholder"
-                value=""
-                disabled={selectedOption !== undefined}
-              >
-                Select Option
-              </option>
-              {subject.map((subject, index) => (
-                <option key={index} value={subject}>
-                  {subject}
-                </option>
-              ))}
-            </select>
-          </form>
-        </div>
+        <label> {label}</label>
+        <select value={value}>
+            <option>Select {label}</option>
+            {options.map((option: IOptions) => {
+                return <option>{option.value}</option>
+            })}
+        </select>
       </div>
     </>
   );
